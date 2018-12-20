4fbc0bf59f488ba9380fbbf95a7ffc57cb85e594
donovanky <donovanky@my.easternct.edu>
2018-12-20 15:02:56 -0500

    Added room list cycle

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
+
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import * as firebase from 'firebase';
+import RoomList from './components/RoomList';
 import './App.css';
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCgmsoP8iMmnIaDX3vZxgvn5DMVR0J5Hqg",
+    authDomain: "bloc-chat-react-205a4.firebaseapp.com",
+    databaseURL: "https://bloc-chat-react-205a4.firebaseio.com",
+    projectId: "bloc-chat-react-205a4",
+    storageBucket: "bloc-chat-react-205a4.appspot.com",
+    messagingSenderId: "612577108674"
+  };
+  firebase.initializeApp(config);
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+  class App extends Component {
+    render() {
+      return (
+        <div className="App">
+          <RoomList firebase={ firebase }/>
+          <div>Message component goes here</div>
+        </div>
+      );
+    }
   }
-}
-
-export default App;
+  export default App;
---
+++ src/components/RoomList.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import '../RoomList.css';
+
+class RoomList extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      rooms: []
+    };
+
+    this.roomsRef = this.props.firebase.database().ref('rooms');
+  }
+
+  componentDidMount() {
+    this.roomsRef.on('child_added', snapshot => {
+      const room = snapshot.val();
+      room.key = snapshot.key;
+      this.setState({ rooms: this.state.rooms.concat( room ) })
+    });
+  }
+
+  render() {
+    return(
+      <nav className="container">
+        <h1>Bloc Chat</h1>
+        {
+          this.state.rooms.map((room, index) =>
+            <div key={ index }><h3>{ room.name }</h3></div>
+          )
+        }
+      </nav>
+    );
+  }
+}
+
+export default RoomList;
