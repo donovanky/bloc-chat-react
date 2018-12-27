55bc07610c177c7f8338139466b9e46eb3a0dd6f
donovanky <donovanky@my.easternct.edu>
2018-12-27 12:28:18 -0500

    Messages work now

--- src/App.js
+++ src/App.js
@@ -1,28 +1,62 @@
 import React, { Component } from 'react';
-import { Route, Link } from 'react-router-dom';
+import * as firebase from 'firebase';
+import RoomList from './components/RoomList';
+import MessageList from './components/MessageList';
+import User from './components/User';
 import './App.css';
-import Landing from './components/Landing';
-import Library from './components/Library';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header>
-          <nav>
-            <Link to '/'>Landing</Link>
-            <Link to '/library'>Library</Link>
-          </nav>
-          <h1>Bloc Jams</h1>
-        </header>
 
-        <main>
-          <Route exact path="/" component={Landing} />
-          <Route path="/library" component={Library} />
-        </main>
-      </div>
-    );
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCgmsoP8iMmnIaDX3vZxgvn5DMVR0J5Hqg",
+    authDomain: "bloc-chat-react-205a4.firebaseapp.com",
+    databaseURL: "https://bloc-chat-react-205a4.firebaseio.com",
+    projectId: "bloc-chat-react-205a4",
+    storageBucket: "bloc-chat-react-205a4.appspot.com",
+    messagingSenderId: "612577108674"
+  };
+  firebase.initializeApp(config);
+
+  class App extends Component {
+    constructor(){
+      super();
+
+      this.state = {
+        activeRoom: '',
+        activeRoomTitle: '',
+        user: ''
+      };
+    }
+
+    selectActiveRoom(key, title){
+      const selectedActiveRoom = key;
+      const newActiveRoomTitle= title;
+      this.setState({ activeRoom: selectedActiveRoom, activeRoomTitle: newActiveRoomTitle});
+    }
+
+    setUser(user) {
+      console.log(`setUser() activated`);
+        this.setState({user: user });
+    }
+
+    render() {
+      return (
+        <div className="App">
+          <RoomList
+            firebase={ firebase }
+            activeRoom= { this.state.activeRoom }
+            selectActiveRoom= { (key, title) => this.selectActiveRoom(key, title) }
+            user= { this.state.user }
+            setUser= { (user) => this.setUser(user) }
+          />
+          <MessageList
+            firebase= { firebase }
+            activeRoom= { this.state.activeRoom}
+            selectedActiveRoom= { this.state.activeRoomTitle }
+            user= { this.state.user }
+          />
+        </div>
+      );
+    }
   }
-}
-
-export default App;
+  export default App;
